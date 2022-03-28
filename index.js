97191a6a9f759eb01f4f4074e4c8c4814e44190a
Mark <mgalex12@icloud.com>
2022-03-28 13:48:55 -0400

    commit tree b8d1a0112ef6f32898df6c41aef9cb149365fdb0

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+   if (someValue > 42) {
+   return(someValue - 42)
+  } else if (someValue < 42) {
+    return(42 - someValue)
+  }
+}
+
+function distanceFromHqInFeet(someValue) {
+  let blocks = distanceFromHqInBlocks(someValue); 
+  return (blocks * 264)
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    // returns the number of feet traveled
+    if (start < destination) {
+        return(destination - start) * 264
+    } else if(start>destination) {
+        return(start - destination) * 264
+    }
+}
+  
+
+function calculatesFarePrice (start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0
+    } else if (distance > 400 && distance < 2000) {
+        return(.02 * (distance - 400))
+    } else if (distance > 2000 && distance < 2500) {
+        return 25
+    } else {
+        return ("cannot travel that far")
+    }
+
+    }
+    
+    
+    
